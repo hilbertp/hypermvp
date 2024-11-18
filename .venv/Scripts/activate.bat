6c7d6d15acc0afc65ed9a9d1a1f864ce81b378bf
Philipp Hilbert <philipp@hyperion-grid.com>
2024-11-19 00:23:09 +0100

    Deine Commit-Nachricht

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hilbe\hypermvp\.venv
+set VIRTUAL_ENV=G:\hyperMVP\hypermvp\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
